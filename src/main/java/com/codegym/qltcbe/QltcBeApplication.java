483b197c6d8801b38bfc3dfbe28b81eca7d62d21
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-11-01 16:26:17 +0700

    first commit

---
+++ src/main/java/com/codegym/qltcbe/QltcBeApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.qltcbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QltcBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QltcBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/qltcbe/QltcBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.qltcbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QltcBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
