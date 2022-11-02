a2d3540647c24998378f70300b424f9e510aa5bc
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-11-02 15:10:12 +0700

    done config jwt and security

---
+++ src/main/java/com/codegym/qltcbe/service/user/UserService.java
@@ -0,0 +1,42 @@
+package com.codegym.qltcbe.service.user;
+
+import com.codegym.qltcbe.model.entity.User;
+import com.codegym.qltcbe.repo.user.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements IUserService{
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public Iterable<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void remove(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails findByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),user.getRoles());
+    }
+}
